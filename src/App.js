4bb825a86563ad5d8c9ef135d3292e20bf6c28af
josephnwachukwu21 <josephnwachukwu051@gmail.com>
2024-06-24 09:54:58 +0100

    css styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
+
+import React from 'react';
 import './App.css';
 import MovieSearch from './components/MovieSearch';
+import { MovieProvider } from './components/MovieContext';
 
 function App() {
-  return (
-    <div>
-   <MovieSearch/>
-    </div>
-  );
+    return (
+      <div className='component'>
+        <MovieProvider>
+  
+                <MovieSearch />
+      
+        </MovieProvider>
+      </div>
+    );
 }
 
 export default App;
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,29 +1,79 @@
-import React, { useState, useContext } from "react";
-import { MovieContext } from "./MovieContext";
+// import React, { useState, useContext } from "react";
+// import { MovieContext } from "./MovieContext";
 
-function SearchForm() {
-    const [title, setTitle] = useState('');
-    const { setSearchQuery } = useContext(MovieContext);
+// function SearchForm() {
+//     const [title, setTitle] = useState('');
+//     const { setSearchQuery } = useContext(MovieContext);
 
-    const handleChange = (e) => {
-        setTitle(e.target.value);
+//     const handleChange = (e) => {
+//         setTitle(e.target.value);
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         setSearchQuery(title);
+//     };
+
+
+//     return (
+//         <div className="title-search"> 
+//             <form onSubmit={handleSubmit} >
+//                 <input type="text" name="title" value={title} placeholder="Search For Your Movie Here" onChange={handleChange} />
+//                 <button type="submit">Submit</button>
+//             </form>
+        
+//         </div>
+        
+//     )}
+
+//     export default SearchForm
+
+import React, { useContext, useState } from 'react';
+import { MovieContext } from './MovieContext';
+
+const SearchForm = () => {
+    const [showInput, setShowInput] = useState(false);
+  const { setSearchQuery } = useContext(MovieContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Search Query Submitted:", inputValue);  // Debugging log
+    setSearchQuery(inputValue);
+  };
+
+  
+    const handleSearchClick = () => {
+        setShowInput(!showInput);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setSearchQuery(title);
-    };
+  return (
+    <div className="title-search">
 
+<span className="material-symbols-outlined menu">menu</span>
+       <div className="search-setting">
+             <span className="material-symbols-outlined search" onClick={handleSearchClick}>search</span>
+            {showInput && (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Search for a movie..."
+      />
+      <button type="submit">Search</button>
+    </form>
+      )}
+                      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                      <span className="material-symbols-outlined more_horiz">more_horiz</span>
+                  </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="title-search"> 
-            <form onSubmit={handleSubmit} >
-                <input type="text" name="title" value={title} placeholder="Search For Your Movie Here" onChange={handleChange} />
-                <button type="submit">Submit</button>
-            </form>
-        
-        </div>
-        
-    )}
+export default SearchForm;
 
-    export default SearchForm
