257b14aaf99b0c5723366e9975f08453c49b4967
Olamilesi1 <olamilesiadeola111@gmail.com>
2024-06-27 12:34:55 +0100

    Css edit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 
-
-import React from 'react';
 import './App.css';
 import MovieSearch from './components/MovieSearch';
-import { MovieProvider } from './components/MovieContext';
 
 function App() {
-    return (
-      <div className='component'>
-        <MovieProvider>
+  return (
+    <div>
+   <MovieSearch/>
+    </div>
+  );
   
-                <MovieSearch />
-      
-        </MovieProvider>
-      </div>
-    );
 }
 
 export default App;
+
+
+
+// import React from 'react';
+// import './App.css';
+// import MovieSearch from './components/MovieSearch';
+// import { MovieProvider } from './components/MovieContext';
+
+// function App() {
+//     return (
+//       <div className='component'>
+//         <MovieProvider>
+  
+//                 <MovieSearch />
+      
+//         </MovieProvider>
+//       </div>
+//     );
+// }
+
+// export default App;
--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,109 +1,109 @@
-// import React from 'react';
-// import { MovieProvider } from './MovieContext';
-// import SearchForm from "./SearchForm";
-// import MovieList from "./MovieList";
-// import MovieDetail from "./MovieDetail";
-// import { MovieContext } from "./MovieContext";
-
-// function MovieSearch() {
-//     return (
-//         <MovieProvider>
-//             <MovieContent />
-//         </MovieProvider>
-//     );
-// }
-
-// const MovieContent = () => {
-//     const { movieDetail, handleBackToList, imgUrl, favorite, handleFavoriteClick } = React.useContext(MovieContext);
-
-//     return (
-//         <div>
-//             {!movieDetail ? (
-//                 <div>
-//                     <SearchForm />
-//                     <br />
-//                     <MovieList />
-//                 </div>
-//             ) : (
-//                 <>
-//                     <button onClick={handleBackToList}>Back To List</button>
-//                     <MovieDetail movie={movieDetail} imgUrl={imgUrl} onFavoriteClick={handleFavoriteClick} favorite={favorite} />
-//                 </>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default MovieSearch;
-
-import React, { useContext } from 'react';
-import { MovieContext } from './MovieContext';
-import MovieList from './MovieList';
-import MovieDetail from './MovieDetail';
-import SearchForm from './SearchForm';
+import React from 'react';
+import { MovieProvider } from './MovieContext';
+import SearchForm from "./SearchForm";
+import MovieList from "./MovieList";
+import MovieDetail from "./MovieDetail";
+import { MovieContext } from "./MovieContext";
 
 function MovieSearch() {
-  const { genres, moviesByGenre, movieDetail, handleMovieClick, handleBackToList, imgUrl, favorite, handleFavoriteClick, movies, searchQuery} = useContext(MovieContext);
-
-  return (
-    <div className='movie-genre'>
-      <SearchForm />
-      {!movieDetail ? (
-        <>
-          {searchQuery ? (
-            // Display search results
-            <>
-              <h2>Search Results</h2>
-              <MovieList
-                movies={movies}
-                imgUrl={imgUrl}
-                onMovieClick={handleMovieClick}
-                favorite={favorite}
-                onFavoriteClick={handleFavoriteClick}
-              />
-            </>
-          ) : (
-            // Display genres and movies by genre
-            <>
-              {genres.map((genre) => (
-                <div key={genre.id}>
-                  <h2>{genre.name}</h2>
-                  <MovieList
-                    movies={moviesByGenre[genre.name] || []}
-                    imgUrl={imgUrl}
-                    onMovieClick={handleMovieClick}
-                    favorite={favorite}
-                    onFavoriteClick={handleFavoriteClick}
-                  />
-                </div>
-              ))}
-            </>
-          )}
-          <h1>Favourites</h1>
-          <div className="favorite">
-            {favorite.map((movie) => (
-              <div key={movie.id} className="movie" onClick={() => handleMovieClick(movie)}>
-                <img src={`${imgUrl}${movie.poster_path}`} alt={movie.title} />
-                <p>Title: {movie.title}</p>
-                <p>Vote Average: {movie.vote_average}</p>
-              </div>
-            ))}
-          </div>
-        </>
-      ) : (
-        <>
-          <MovieDetail
-            movie={movieDetail}
-            imgUrl={imgUrl}
-            onFavoriteClick={handleFavoriteClick}
-            favorite={favorite}
-          />
-          <button onClick={handleBackToList}>Back To List</button>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <MovieProvider>
+            <MovieContent />
+        </MovieProvider>
+    );
 }
 
+const MovieContent = () => {
+    const { movieDetail, handleBackToList, imgUrl, favorite, handleFavoriteClick } = React.useContext(MovieContext);
+
+    return (
+        <div>
+            {!movieDetail ? (
+                <div>
+                    <SearchForm />
+                    <br />
+                    <MovieList />
+                </div>
+            ) : (
+                <>
+                    <button onClick={handleBackToList}>Back To List</button>
+                    <MovieDetail movie={movieDetail} imgUrl={imgUrl} onFavoriteClick={handleFavoriteClick} favorite={favorite} />
+                </>
+            )}
+        </div>
+    );
+};
+
 export default MovieSearch;
+
+// import React, { useContext } from 'react';
+// import { MovieContext } from './MovieContext';
+// import MovieList from './MovieList';
+// import MovieDetail from './MovieDetail';
+// import SearchForm from './SearchForm';
+
+// function MovieSearch() {
+//   const { genres, moviesByGenre, movieDetail, handleMovieClick, handleBackToList, imgUrl, favorite, handleFavoriteClick, movies, searchQuery} = useContext(MovieContext);
+
+//   return (
+//     <div className='movie-genre'>
+//       <SearchForm />
+//       {!movieDetail ? (
+//         <>
+//           {searchQuery ? (
+//             // Display search results
+//             <>
+//               <h2>Search Results</h2>
+//               <MovieList
+//                 movies={movies}
+//                 imgUrl={imgUrl}
+//                 onMovieClick={handleMovieClick}
+//                 favorite={favorite}
+//                 onFavoriteClick={handleFavoriteClick}
+//               />
+//             </>
+//           ) : (
+//             // Display genres and movies by genre
+//             <>
+//               {genres.map((genre) => (
+//                 <div key={genre.id}>
+//                   <h2>{genre.name}</h2>
+//                   <MovieList
+//                     movies={moviesByGenre[genre.name] || []}
+//                     imgUrl={imgUrl}
+//                     onMovieClick={handleMovieClick}
+//                     favorite={favorite}
+//                     onFavoriteClick={handleFavoriteClick}
+//                   />
+//                 </div>
+//               ))}
+//             </>
+//           )}
+//           <h1>Favourites</h1>
+//           <div className="favorite">
+//             {favorite.map((movie) => (
+//               <div key={movie.id} className="movie" onClick={() => handleMovieClick(movie)}>
+//                 <img src={`${imgUrl}${movie.poster_path}`} alt={movie.title} />
+//                 <p>Title: {movie.title}</p>
+//                 <p>Vote Average: {movie.vote_average}</p>
+//               </div>
+//             ))}
+//           </div>
+//         </>
+//       ) : (
+//         <>
+//           <MovieDetail
+//             movie={movieDetail}
+//             imgUrl={imgUrl}
+//             onFavoriteClick={handleFavoriteClick}
+//             favorite={favorite}
+//           />
+//           <button onClick={handleBackToList}>Back To List</button>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default MovieSearch;
 
