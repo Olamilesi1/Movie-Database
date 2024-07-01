3b769105a0cd138360ef0ef281d6d89030347e8b
Olamilesi1 <olamilesiadeola111@gmail.com>
2024-07-01 18:42:57 +0100

    Implemented the locate by genre using the menu bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 
-import './App.css';
-import MovieSearch from './components/MovieSearch';
-
-function App() {
-  return (
-    <div>
-   <MovieSearch/>
-    </div>
-  );
-  
-}
-
-export default App;
-
-
-
-// import React from 'react';
 // import './App.css';
 // import MovieSearch from './components/MovieSearch';
-// import { MovieProvider } from './components/MovieContext';
 
 // function App() {
-//     return (
-//       <div className='component'>
-//         <MovieProvider>
+//   return (
+//     <div>
+//    <MovieSearch/>
+//     </div>
+//   );
   
-//                 <MovieSearch />
-      
-//         </MovieProvider>
-//       </div>
-//     );
 // }
 
 // export default App;
+
+
+
+import React from 'react';
+import './App.css';
+import MovieSearch from './components/MovieSearch';
+import { MovieProvider } from './components/MovieContext';
+
+function App() {
+    return (
+      <div className='component'>
+        <MovieProvider>
+  
+                <MovieSearch />
+      
+        </MovieProvider>
+      </div>
+    );
+}
+
+export default App;
--- src/components/MovieContext.jsx
+++ src/components/MovieContext.jsx
@@ -1,195 +1,200 @@
-import React, { createContext, useState, useEffect } from 'react';
-
-const MovieContext = createContext();
-
-const MovieProvider = ({ children }) => {
-    const [movies, setMovies] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [movieDetail, setMovieDetail] = useState(null);
-    const [favorite, setFavorite] = useState([]);
-
-    const imgUrl = "https://image.tmdb.org/t/p/w500";
-    const apiKey = "c7cba54e8036127f06f9792f9781dbad";
-    const baseUrl = "https://api.themoviedb.org/3";
-    const apiUrl = `${baseUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
-    const searchUrl = `${baseUrl}/search/movie?api_key=${apiKey}&query=`;
-
-    useEffect(() => {
-        fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => setMovies(data.results))
-            .catch((error) => console.error("Error fetching movies:", error));
-    }, []);
-
-    useEffect(() => {
-        if (searchQuery) {
-            fetch(searchUrl + searchQuery)
-                .then((response) => response.json())
-                .then((data) => setMovies(data.results))
-                .catch((error) => console.error("Error searching movies:", error));
-        }
-    }, [searchQuery]);
-
-    useEffect(() => {
-        const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
-        if (storedFavorites) {
-            setFavorite(storedFavorites);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (favorite.length > 0) {
-            localStorage.setItem('favorites', JSON.stringify(favorite));
-        } else {
-            localStorage.removeItem('favorites');
-        }
-    }, [favorite]);
-
-    const handleMovieClick = (movie) => {
-        setMovieDetail(movie);
-    };
-
-    const handleBackToList = () => {
-        setMovieDetail(null);
-    };
-
-    const handleFavoriteClick = (movie) => {
-        if (!favorite.some(fav => fav.id === movie.id)) {
-            setFavorite([...favorite, movie]);
-        } else {
-            setFavorite(favorite.filter(fav => fav.id !== movie.id));
-        }
-    };
-
-    return (
-        <MovieContext.Provider value={{ movies, setSearchQuery, movieDetail, handleMovieClick, handleBackToList, favorite, handleFavoriteClick, imgUrl }}>
-            {children}
-        </MovieContext.Provider>
-    );
-};
-
-export { MovieContext, MovieProvider };
-
-
-// import React, { createContext, useState, useEffect } from "react";
+// import React, { createContext, useState, useEffect } from 'react';
 
 // const MovieContext = createContext();
 
 // const MovieProvider = ({ children }) => {
-//   const [movies, setMovies] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [movieDetail, setMovieDetail] = useState(null);
-//   const [favorite, setFavorite] = useState([]);
-//   const [genres, setGenres] = useState([]);
-//   const [moviesByGenre, setMoviesByGenre] = useState({});
+//     const [movies, setMovies] = useState([]);
+//     const [searchQuery, setSearchQuery] = useState('');
+//     const [movieDetail, setMovieDetail] = useState(null);
+//     const [favorite, setFavorite] = useState([]);
 
-//   const imgUrl = "https://image.tmdb.org/t/p/w500";
-//   const apiKey = "c7cba54e8036127f06f9792f9781dbad";
-//   const baseUrl = "https://api.themoviedb.org/3";
-//   const apiUrl = `${baseUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
-//   const searchUrl = `${baseUrl}/search/movie?api_key=${apiKey}&query=`;
-//   const genresUrl = `${baseUrl}/genre/movie/list?api_key=${apiKey}`;
+//     const imgUrl = "https://image.tmdb.org/t/p/w500";
+//     const apiKey = "c7cba54e8036127f06f9792f9781dbad";
+//     const baseUrl = "https://api.themoviedb.org/3";
+//     const apiUrl = `${baseUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
+//     const searchUrl = `${baseUrl}/search/movie?api_key=${apiKey}&query=`;
 
-//   useEffect(() => {
-//     fetch(apiUrl)
-//       .then((response) => response.json())
-//       .then((data) => setMovies(data.results))
-//       .catch((error) => console.error("Error fetching movies:", error));
-      
-//       fetch(genresUrl)
-//         .then((response) => response.json())
-//         .then((data) => setGenres(data.genres))
-//         .catch((error) => console.error("Error fetching genres:", error));
-//   }, []);
+//     useEffect(() => {
+//         fetch(apiUrl)
+//             .then((response) => response.json())
+//             .then((data) => setMovies(data.results))
+//             .catch((error) => console.error("Error fetching movies:", error));
+//     }, []);
 
-//   useEffect(() => {
-//     if (searchQuery) {
-//       console.log("Search Query Changed:", searchQuery);
-//       console.log("Fetching search results for:", searchQuery);
-//       fetch(searchUrl + searchQuery)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           setMovies(data.results);
-//           console.log("Search Results:", data.results);
-//         })
-//         .catch((error) => console.error("Error searching movies:", error));
-//     } else {
-//       fetch(apiUrl)
-//         .then((response) => response.json())
-//         .then((data) => setMovies(data.results))
-//         .catch((error) => console.error("Error fetching movies:", error));
-//     }
-//   }, [searchQuery]);
+//     useEffect(() => {
+//         if (searchQuery) {
+//             fetch(searchUrl + searchQuery)
+//                 .then((response) => response.json())
+//                 .then((data) => setMovies(data.results))
+//                 .catch((error) => console.error("Error searching movies:", error));
+//         }
+//     }, [searchQuery]);
 
-//   useEffect(() => {
-//     const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-//     if (storedFavorites) {
-//       setFavorite(storedFavorites);
-//     }
-//   }, []);
+//     useEffect(() => {
+//         const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
+//         if (storedFavorites) {
+//             setFavorite(storedFavorites);
+//         }
+//     }, []);
 
-//   useEffect(() => {
-//     if (favorite.length > 0) {
-//       localStorage.setItem("favorites", JSON.stringify(favorite));
-//     } else {
-//       localStorage.removeItem("favorites");
-//     }
-//   }, [favorite]);
+//     useEffect(() => {
+//         if (favorite.length > 0) {
+//             localStorage.setItem('favorites', JSON.stringify(favorite));
+//         } else {
+//             localStorage.removeItem('favorites');
+//         }
+//     }, [favorite]);
 
-//   useEffect(() => {
-//     const fetchMoviesByGenre = async () => {
-//       const genreMovies = {};
-//       for (let genre of genres) {
-//         const url = `${baseUrl}/discover/movie?with_genres=${genre.id}&api_key=${apiKey}`;
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         genreMovies[genre.name] = data.results;
-//       }
-//       setMoviesByGenre(genreMovies);
+//     const handleMovieClick = (movie) => {
+//         setMovieDetail(movie);
 //     };
 
-//     if (genres.length > 0) {
-//       fetchMoviesByGenre();
-//     }
-//   }, [genres]);
+//     const handleBackToList = () => {
+//         setMovieDetail(null);
+//     };
 
-//   const handleMovieClick = (movie) => {
-//     setMovieDetail(movie);
-//   };
+//     const handleFavoriteClick = (movie) => {
+//         if (!favorite.some(fav => fav.id === movie.id)) {
+//             setFavorite([...favorite, movie]);
+//         } else {
+//             setFavorite(favorite.filter(fav => fav.id !== movie.id));
+//         }
+//     };
 
-//   const handleBackToList = () => {
-//     setMovieDetail(null);
-//   };
-
-//   const handleFavoriteClick = (movie) => {
-//     if (!favorite.some((fav) => fav.id === movie.id)) {
-//       setFavorite([...favorite, movie]);
-//     } else {
-//       setFavorite(favorite.filter((fav) => fav.id !== movie.id));
-//     }
-//   };
-
-//   return (
-//     <MovieContext.Provider
-//       value={{
-//         movies,
-//         genres,
-//         moviesByGenre,
-//         movieDetail,
-//         handleMovieClick,
-//         handleBackToList,
-//         favorite,
-//         handleFavoriteClick,
-//         imgUrl,
-//         searchQuery,
-//         setSearchQuery,
-//       }}
-//     >
-//       {children}
-//     </MovieContext.Provider>
-//   );
+//     return (
+//         <MovieContext.Provider value={{ movies, setSearchQuery, movieDetail, handleMovieClick, handleBackToList, favorite, handleFavoriteClick, imgUrl }}>
+//             {children}
+//         </MovieContext.Provider>
+//     );
 // };
 
 // export { MovieContext, MovieProvider };
+
+
+import React, { createContext, useState, useEffect } from "react";
+
+const MovieContext = createContext();
+
+const MovieProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movieDetail, setMovieDetail] = useState(null);
+  const [favorite, setFavorite] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [moviesByGenre, setMoviesByGenre] = useState({});
+  const [theme, setTheme] = useState('light'); 
+
+  const imgUrl = "https://image.tmdb.org/t/p/w500";
+  const apiKey = "c7cba54e8036127f06f9792f9781dbad";
+  const baseUrl = "https://api.themoviedb.org/3";
+  const apiUrl = `${baseUrl}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
+  const searchUrl = `${baseUrl}/search/movie?api_key=${apiKey}&query=`;
+  const genresUrl = `${baseUrl}/genre/movie/list?api_key=${apiKey}`;
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.error("Error fetching movies:", error));
+      fetch(genresUrl)
+        .then((response) => response.json())
+        .then((data) => setGenres(data.genres))
+        .catch((error) => console.error("Error fetching genres:", error));
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      console.log("Search Query Changed:", searchQuery);
+      console.log("Fetching search results for:", searchQuery);
+      fetch(searchUrl + searchQuery)
+        .then((response) => response.json())
+        .then((data) => {
+          setMovies(data.results);
+          console.log("Search Results:", data.results);
+        })
+        .catch((error) => console.error("Error searching movies:", error));
+    } else {
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => setMovies(data.results))
+        .catch((error) => console.error("Error fetching movies:", error));
+    }
+  }, [searchQuery]);
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    if (storedFavorites) {
+      setFavorite(storedFavorites);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (favorite.length > 0) {
+      localStorage.setItem("favorites", JSON.stringify(favorite));
+    } else {
+      localStorage.removeItem("favorites");
+    }
+  }, [favorite]);
+
+  useEffect(() => {
+    const fetchMoviesByGenre = async () => {
+      const genreMovies = {};
+      for (let genre of genres) {
+        const url = `${baseUrl}/discover/movie?with_genres=${genre.id}&api_key=${apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        genreMovies[genre.name] = data.results;
+      }
+      setMoviesByGenre(genreMovies);
+    };
+
+    if (genres.length > 0) {
+      fetchMoviesByGenre();
+    }
+  }, [genres]);
+
+  const handleMovieClick = (movie) => {
+    setMovieDetail(movie);
+  };
+
+  const handleBackToList = () => {
+    setMovieDetail(null);
+  };
+
+  const handleFavoriteClick = (movie) => {
+    if (!favorite.some((fav) => fav.id === movie.id)) {
+      setFavorite([...favorite, movie]);
+    } else {
+      setFavorite(favorite.filter((fav) => fav.id !== movie.id));
+    }
+  };
+  // console.log(movies)
+  return (
+    <MovieContext.Provider
+      value={{
+        movies,
+        genres,
+        moviesByGenre,
+        movieDetail,
+        handleMovieClick,
+        handleBackToList,
+        favorite,
+        handleFavoriteClick,
+        imgUrl,
+        searchQuery,
+        setSearchQuery,
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+};
+
+export { MovieContext, MovieProvider };
 
 
